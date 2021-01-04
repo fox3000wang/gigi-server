cb0c047d655f654ceb13d8f7bbdc8b6555106de2
fox.wang <fox3000wang@hotmail.com>
2021-01-04 09:41:30 +0800

    post流程跑通

--- src/vita/vita.controller.ts
+++ src/vita/vita.controller.ts
@@ -1,12 +1,17 @@
 import { VitaService } from './vita.service';
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 
 @Controller('vita')
 export class VitaController {
   constructor(private readonly vitaService: VitaService) {}
 
   @Get()
-  getLibrary() {
+  getLibrary():any {
     return this.vitaService.getLibrary();
+  }
+
+  @Post()
+  addActiveRecord(@Body() record:any):any{
+    return this.vitaService.recordActive(record);
   }
 }
--- src/vita/vita.service.ts
+++ src/vita/vita.service.ts
@@ -1,22 +1,44 @@
 import fs =  require('fs');
 import path =  require('path');
-
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class VitaService {
   
-  private readonly fileParh = path.join(__dirname, `../../data/chinese.json`);
   private library:Array<object> = [];
 
   getLibrary(): any {
+    const fileParh:string = path.join(__dirname, `../../data/chinese.json`);
     if(this.library['chinese']){
       return this.library['chinese'];
     }else{
-      fs.readFile(this.fileParh, (err, data)=>{
+      fs.readFile(fileParh, (err, data)=>{
         err && console.error(err);
         this.library['chinese'] = JSON.parse(data.toString());
       });
     }
+  }
+
+  recordActive(record:any): any {
+    const fileParh:string = path.join(__dirname, `../../data/record.json`);
+
+    function closeFile(err, fd) {
+      err && console.error(err);
+      fs.close(fd, () => console.log('done'));
+    }
+
+    fs.readFile(fileParh, (err, data)=>{
+      err && console.error(err);
+      const records:Array<object> = JSON.parse(data.toString());
+      records.push(record);
+
+      console.log(record);
+
+      fs.open(fileParh, 'w', (err, fd) => {
+        err && console.error(err);
+        fs.write(fd, JSON.stringify(records), closeFile);
+      });
+
+    });
   }
 }
