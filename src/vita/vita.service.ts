39cdaed4830bea5c593405e6a4fecf4045fb42fa
fox.wang <fox3000wang@hotmail.com>
2022-11-12 18:46:29 +0800

    数据从数据库拿

--- src/vita/vita.service.ts
+++ src/vita/vita.service.ts
@@ -1,34 +1,69 @@
-import fs =  require('fs');
-import path =  require('path');
+import fs = require('fs');
+import path = require('path');
 import { Injectable } from '@nestjs/common';
 
 type record = {
-  id:number,
-  name: string,
-  URI:string,
-  result:string,
-  date:string,
-  language:string,
-}
+  id: number;
+  name: string;
+  URI: string;
+  result: string;
+  date: string;
+  language: string;
+};
 
 @Injectable()
 export class VitaService {
-  
-  getRecord():any{
-    const fileParhCn:string = path.join(__dirname, `../../data/record.cn.json`);
-    const dataCn = fs.readFileSync(fileParhCn);
-    const recordCn = JSON.parse(dataCn.toString())
-
-    const fileParhEn = path.join(__dirname, `../../data/record.en.json`);
-    const dataEn = fs.readFileSync(fileParhEn);
-    const recordEn = JSON.parse(dataEn.toString())
-    
-    return {recordCn, recordEn};
+  pool: any;
+  constructor() {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const mysql = require('mysql2');
+    this.pool = mysql.createPool({
+      host: 'wangzm.cn',
+      user: 'user',
+      password: 'useruser',
+      database: 'gigi',
+      charset: 'utf8',
+    });
   }
 
-  postRecord(record:record): any {
-    if(!record){
-      return 'record is null'
+  // 封装成可以同步执行的函数
+  query(sql) {
+    return new Promise((resolve, reject) => {
+      this.pool.getConnection(function(err, connection) {
+        if (err) {
+          reject(err);
+        } else {
+          connection.query(sql, (err, rows) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(rows);
+            }
+            // 结束会话
+            connection.release();
+          });
+        }
+      });
+    });
+  }
+
+  async getRecord() {
+    const sql = `SELECT * FROM record WHERE lang='cn';`;
+    let rows = await this.query(sql);
+    const recordCn = JSON.stringify(rows);
+
+    const sqlEn = `SELECT * FROM record WHERE lang='en';`;
+    rows = await this.query(sqlEn);
+    const recordEn = JSON.stringify(rows);
+
+    return { recordCn, recordEn };
+  }
+
+  //return {recordCn, recordEn};
+
+  postRecord(record: record): any {
+    if (!record) {
+      return 'record is null';
     }
 
     function closeFile(err, fd) {
@@ -37,35 +72,37 @@ export class VitaService {
     }
 
     console.log(record);
-    const fileParh:string = path.join(__dirname, `../../data/record.${record.language}.json`);
-    
+    const fileParh: string = path.join(
+      __dirname,
+      `../../data/record.${record.language}.json`,
+    );
+
     const data = fs.readFileSync(fileParh);
-    const records:Array<record> = JSON.parse(data.toString());
-    
+    const records: Array<record> = JSON.parse(data.toString());
+
     const newRecord = {
       ...record,
-      date: new Date().toLocaleDateString()
+      date: new Date().toLocaleDateString(),
     };
 
-    if(this.hasRecord(newRecord, records)){
+    if (this.hasRecord(newRecord, records)) {
       console.error(`has same data!`);
       return;
     }
 
     records.push(newRecord);
     console.log(record);
-    
+
     fs.open(fileParh, 'w', (err, fd) => {
       err && console.error(err);
       fs.write(fd, JSON.stringify(records), closeFile);
     });
-
   }
 
-  hasRecord(record:record, records:Array<record>):boolean{
+  hasRecord(record: record, records: Array<record>): boolean {
     let result = false;
-    records.forEach( (r:record) => {
-      if(record.id === r.id && record.date === r.date){
+    records.forEach((r: record) => {
+      if (record.id === r.id && record.date === r.date) {
         result = true;
       }
     });
@@ -85,6 +122,4 @@ export class VitaService {
   //     });
   //   }
   // }
-
-  
 }
